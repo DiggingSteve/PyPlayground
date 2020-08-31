d95c61ed3e880e70aad8ba9d61e33e5ab778d5b0
Steve <doublefly@live.cn>
2020-08-31 18:27:42 +0800

    逆序数 关键点在于当满足逆序条件时 累加左边剩余项目

--- LinearAlgebra/det.py
+++ LinearAlgebra/det.py
@@ -1,42 +1,39 @@
-# 逆序数
-def getInversion(numlist):
-    count = 0
-    for i in range(1,len(numlist)):
-        subscript = numlist[i]
-        for j in range(i):
-            if subscript < numlist[j]:
-                count += 1
-    return count
-D = 0
-# 全排列，求每项的积
-def permutation(dd,ilist,jlist,index):
-    global D
-    for i in range(index,len(jlist)):
-        if index == len(jlist)-1:
-            term = 1
-            for ii in range(len(ilist)):
-                i = ilist[ii]
-                j = jlist[ii]
-                term *= dd[i][j]
-            if getInversion(jlist) % 2 == 0:
-                D += term
-            else:D -= term
-            return
-        tmp = jlist[index]
-        jlist[index] = jlist[i]
-        jlist[i] = tmp
-        permutation(dd,ilist,jlist,index+1)
-        tmp = jlist[index]
-        jlist[index] = jlist[i]
-        jlist[i] = tmp
- 
-if __name__ == '__main__':
-    dd = [[1, 2, -4], [-2, 2, 1], [-3, 4, -2]]
-    dd =[[1,1,1,1],[-1,2,1,3],[1,4,1,9],[-1,8,1,27]]
-    jlist = []
-    ilist = []
-    for ii in range(len(dd)):
-        ilist.append(ii)
-        jlist.append(ii)
-    permutation(dd,ilist,jlist,0)
-    print(D)
+# 逆序数常规求法 1 暴力 n^2 不推荐  2 在归并排序中 记录左右序列交换次数 即为逆序总合 行列式公式 -1^t ∑a1p1 a2p2...anpn
+count = 0
+
+
+def merge(left, right):
+    i = j = 0
+    temp = []
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            temp.append(left[i])
+            i = i+1
+        else:
+            temp.append(right[j])
+            global count
+            count+=len(left)-i #右边有个小的 也就是说左边剩下的都是逆序
+            j += 1
+    if(i < len(left)):
+        temp.extend(left[i:])
+    else:
+        temp.extend(right[j:])
+
+    return temp
+
+ # 分到只有一个序列的 递归深度为log2^n
+
+
+def merge_sort(list):
+
+    if(len(list) <= 1):
+        return list
+    mid = len(list) >> 1
+    left = merge_sort(list[:mid])
+    right = merge_sort(list[mid:])
+    return merge(left, right)
+
+
+merge_sort([2,1])
+print(count)
+print(1+2+3+4+5+6+7+8)
